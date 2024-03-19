5ce0abf5470ceaf54d4e17287ab528835d21a2f2
Tom Van den Bulck <159095052+tvanden3@users.noreply.github.com>
2024-03-19 03:06:19 +0000

    This commit includes search by cast

--- query.py
+++ query.py
@@ -10,7 +10,20 @@ def movie_list():
     for i in range(len(movies._movies)):
         print(movies._movies[i]['name'])
     print()
-#def cast_search(answer)
+def cast_search(answer):
+    answer = str(answer).lower()
+    for i in range(len(movies._movies)):
+        castFound = False
+        for j in range(len(movies._movies[i]['cast'])):
+            if movies._movies[i]['cast'][j].lower().__contains__(answer):
+                castFound = True
+        if castFound:
+            print(movies._movies[i]['name'])
+            print("[", end = ' ')
+            for k in range(0,len(movies._movies[i]['cast'])):
+                if movies._movies[i]['cast'][k].lower().__contains__(answer):
+                    print(movies._movies[i]['cast'][k], end = ' ')
+            print("]")
 choice = ' '
 
 
